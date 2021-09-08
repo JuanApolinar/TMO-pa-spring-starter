ca22501993a133de54e18e9f6ca1be549e55bff3
Juan Apolinar <juanapolinar@my.unt.edu>
2021-09-08 16:39:15 -0500

    first commit

--- src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
@@ -1,13 +1,31 @@
 package com.galvanize.tmo.paspringstarter;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 public class LibraryController {
 
-    @GetMapping("/health")
-    public void health() {
+    List<Book> bookList = new ArrayList<>();
 
+
+    @GetMapping(value="/api/books", produces = "application/json")
+    public List<Book> health() {
+        return bookList;
+    }
+
+    @PostMapping(value = "/api/books", consumes = "application/json", produces = "application/json")
+    public Book postBooks(@RequestBody Book newBook){
+        newBook.setId(bookList.size() + 1);
+        bookList.add(newBook);
+        return newBook;
+    }
+
+    @DeleteMapping("/api/books")
+    public void deleteBooks(){
+        bookList.clear();
     }
 }
